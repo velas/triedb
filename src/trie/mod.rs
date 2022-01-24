0cd4718435521cb7960bf21f85bb16eedaab2433
Maksim Vykhota <m.vykhota@protonmail.com>
2022-01-24 13:15:22 +0300

    Apply `gc` internals to `trie` interface

--- src/trie/mod.rs
+++ src/trie/mod.rs
@@ -1,14 +1,24 @@
+#![allow(dead_code)] // FIXME: delete this lint
 use std::collections::HashMap;
 
 use super::H256;
 use crate::database::{Database, DatabaseMut};
 use crate::empty_trie_hash;
+use crate::Change;
 
 pub mod typed;
+
+#[derive(Default)]
+pub struct DatabaseTrieMutPatch {
+    pub root: H256,
+    pub change: Change,
+}
 
 pub struct TrieHandle<D> {
     database: D,
     root: H256,
+    change: Change,
+    change_data: HashMap<H256, Vec<u8>>,
 }
 
 impl<D: Default> Default for TrieHandle<D> {
@@ -16,13 +26,17 @@ impl<D: Default> Default for TrieHandle<D> {
         Self {
             database: Default::default(),
             root: empty_trie_hash!(),
+            change: Change::default(),
+            change_data: HashMap::default()
         }
     }
 }
 
 impl<D> TrieHandle<D> {
     pub fn new(database: D, root: H256) -> Self {
-        Self { database, root }
+        let change = Change::default();
+        let change_data = HashMap::default();
+        Self { database, root, change, change_data }
     }
 
     pub fn root(&self) -> H256 {
@@ -32,18 +46,18 @@ impl<D> TrieHandle<D> {
         &self.database
     }
 }
-impl<D: DatabaseMut> TrieHandle<D> {
+impl<D: Database + DatabaseMut> TrieHandle<D> {
     pub fn insert(&mut self, key: &[u8], value: &[u8]) {
         let (new_root, change) = crate::insert(self.root, &self.database, key, value);
 
-        self.database.apply_change(change);
+        self.merge(&change);
         self.root = new_root;
     }
 
     pub fn delete(&mut self, key: &[u8]) {
         let (new_root, change) = crate::delete(self.root, &self.database, key);
 
-        self.database.apply_change(change);
+        self.merge(&change);
         self.root = new_root;
     }
 
@@ -58,9 +72,49 @@ impl<D: DatabaseMut> TrieHandle<D> {
         let (new_root, change) = crate::build(map);
 
         let mut ret = Self::default();
-        ret.database.apply_change(change);
+        ret.merge(&change);
         ret.root = new_root;
 
         ret
+    }
+
+    pub fn merge(&mut self, change: &Change) {
+        for (key, v) in &change.changes {
+            if let Some(v) = v {
+                self.change_data.insert(*key, v.clone());
+            } else {
+                self.change_data.remove(key);
+            }
+        }
+        self.change.merge(change)
+    }
+
+    pub fn into_patch(self) -> DatabaseTrieMutPatch {
+        let Self {
+            root,
+            change,
+            change_data,
+            ..
+        } = self;
+        // ideally we need map ordered by push time, but currently we use log+map so we need
+        // filter changes that was removed during latest insert, collect only changes that is equal to actual.
+        let changes = change
+            .changes
+            .into_iter()
+            .filter(|(k, v)| v.is_some() == change_data.get(k).is_some())
+            .collect();
+        DatabaseTrieMutPatch {
+            root,
+            change: Change { changes },
+        }
+    }
+
+    pub fn trie_for(db: D, root: H256) -> Self {
+        Self {
+            database: db,
+            change: Change::default(),
+            change_data: Default::default(),
+            root,
+        }
     }
 }
--- src/trie/typed.rs
+++ src/trie/typed.rs
@@ -3,14 +3,14 @@ use sha3::{Digest, Keccak256};
 use super::*;
 use std::{borrow::Cow, marker::PhantomData};
 
-trait KeyFamily<K> {
+pub trait KeyFamily<K> {
     fn calc_key(key: &K) -> Cow<[u8]>;
 }
 
 pub enum Secure {}
 pub enum Raw {}
 
-trait ValueFamily<V> {
+pub trait ValueFamily<V> {
     fn encode_value(value: &V) -> Cow<[u8]>;
 }
 pub enum Encoded {}
@@ -44,7 +44,7 @@ pub struct TypedTrieHandle<K, V, D> {
     handle: TrieHandle<D>,
 }
 
-impl<D: DatabaseMut, KF, VF> TypedTrieHandle<KF, VF, D> {
+impl<D: Database + DatabaseMut, KF, VF> TypedTrieHandle<KF, VF, D> {
     /// Into the underlying TrieMut object.
     pub fn inner(self) -> TrieHandle<D> {
         self.handle
